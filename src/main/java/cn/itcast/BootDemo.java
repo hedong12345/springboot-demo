f7d1ada6ae94d6057cfe6b6ad813d0e415f0c904
hd <1173081990@qq.com>
2019-12-21 23:42:51 +0800

    整合mybatis和通用mapper

--- src/main/java/cn/itcast/BootDemo.java
+++ src/main/java/cn/itcast/BootDemo.java
@@ -2,11 +2,13 @@ package cn.itcast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * 启动项目
  */
 @SpringBootApplication
+@MapperScan("cn.itcast.mapper")
 public class BootDemo {
     public static void main(String[] args) {
         SpringApplication.run(BootDemo.class,args);
